6d2f9b42c7bc0c582065cb8f76945c253c523502
huxuem <1181682664@qq.com>
2023-11-13 09:31:35 +0800

    小改，把那个相机转动关了

--- Assets/Scripts/Comet.cs
+++ Assets/Scripts/Comet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 //using System.Diagnostics;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+//using static UnityEditor.PlayerSettings;
 
 public class Comet : MonoBehaviour
 {
